86e77cefd7b01025929333116c942c62dbd344cc
ahmed <ahmedramim821@gmail.com>
2023-05-03 00:39:54 +0600

    fetch data for home url

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
-
+const cors = require('cors')
 const chefs = require('./data/chefs.json')
 app.use(cors())
 
 app.get('/',(req,res) => {
     res.send(chefs)
 })
+app.get('/chefsdetails/:id',(req,res)=> {
+    const id = req.params.id;
+    const chefsDetails = chefs.find(ch => ch.id === id)
+    res.send(chefsDetails)
+})
 
-// app.listen(port,)
+app.listen(port,()=>console.log('assaignment is so hard'))
